cd3d3d6644728dec8cb54c0e4cadc497cd28e029
JYL <jyl7094@gmail.com>
2022-03-03 14:16:09 -0500

    Complete implementing array

---
+++ array.py
@@ -0,0 +1,41 @@
+class Array:
+    def __init__(self):
+        self._length = 0
+        self._data = {}
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def get(self, index):
+        return self._data[index]
+    
+    def push(self, item):
+        self._data[self._length] = item
+        self._length += 1
+        return self._length
+    
+    def pop(self):
+        last_item = self._data[self._length-1]
+        del self._data[self._length-1]
+        self._length -= 1
+        return last_item
+
+    def delete(self, index):
+        item = self._data[index]
+        for i in range(index, self._length-1):
+            self._data[i] = self._data[i+1]
+            
+        del self._data[self._length-1]
+        self._length -= 1
+        return item
+
+
+ary = Array()
+print(ary.push('hi'))
+print(ary.push('you'))
+print(ary.push('!'))
+print(ary.delete(0))
+print(ary.push('are'))
+print(ary.push('nice'))
+print(ary.delete(1))
+print(ary)
