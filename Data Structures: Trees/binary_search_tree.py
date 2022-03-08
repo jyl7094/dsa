d5b6b45b8a090989717a841806cd3ffec61b3ced
JYL <jyl7094@gmail.com>
2022-03-08 04:27:38 -0500

    Complete binary search tree

--- Data Structures: Trees/binary_search_tree.py
+++ Data Structures: Trees/binary_search_tree.py
@@ -1,113 +1,134 @@
 class Node:
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
     def __str__(self):
-        return f'{{ val: {self.val}, left: {self.left}, right: {self.right} }}'
+        return f'{{val: {self.val}, left: {self.left}, right: {self.right}}}'
 
 
 class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def __str__(self):
+        if self.root is None:
+            return '{val: None, left: None, right: None}'
+        return str(self.traverse(self.root))
+
+    def traverse(self, node):
+        tree = node
+        tree.left = None if node.left is None else self.traverse(node.left)
+        tree.right = None if node.right is None else self.traverse(node.right)
+        return tree
+    
     def insert(self, val):
         if self.root is None:
             self.root = Node(val)
         else:
             curr = self.root
-            while True:
-                if val > curr.val:
-                    if curr.right is None:
-                        curr.right = Node(val)
-                        return True
-                    curr = curr.right
-                elif val < curr.val:
+            while curr:
+                if val < curr.val:
                     if curr.left is None:
                         curr.left = Node(val)
-                        return True
+                        break
                     curr = curr.left
+                elif val > curr.val:
+                    if curr.right is None:
+                        curr.right = Node(val)
+                        break
+                    curr = curr.right
                 else:
                     return False
+        return True
 
     def lookup(self, val):
         if self.root is None:
             return False
         curr = self.root
-        while True:
-            if val > curr.val:
-                if curr.right is None:
-                    return False
-                curr = curr.right
-            elif val < curr.val:
+        while curr:
+            if val < curr.val:
                 if curr.left is None:
                     return False
                 curr = curr.left
+            elif val > curr.val:
+                if curr.right is None:
+                    return False
+                curr = curr.right
             else:
-                return curr
+                return True
 
     def remove(self, val):
-        """
-        Does not support removing the root
-        """
         if self.root is None:
             return False
         curr = self.root
         parent = None
-        while True:
-            if val > curr.val:
-                if curr.right is None:
-                    return False
-                parent = curr
-                curr = curr.right
-            elif val < curr.val:
+        while curr:
+            if val < curr.val:
                 if curr.left is None:
                     return False
                 parent = curr
                 curr = curr.left
+            elif val > curr.val:
+                if curr.right is None:
+                    return False
+                parent = curr
+                curr = curr.right
             else:
-                # case 1: no child
+                # 1: target node is a leaf
                 if curr.left is None and curr.right is None:
-                    if parent.left == curr:
+                    if parent is None:
+                        self.root = None
+                    elif parent.left == curr:
                         parent.left = None
                     else:
                         parent.right = None
-                # case 2: one child
-                elif curr.left is None and curr.right is not None:
-                    if parent.left == curr:
-                        parent.left = curr.right
-                    else:
-                        parent.right = curr.right
-                elif curr.left is not None and curr.right is None:
-                    if parent.left == curr:
+                # 2: target node has a child
+                elif curr.left and curr.right is None:
+                    if parent is None:
+                        self.root = curr.left
+                    elif parent.left == curr:
                         parent.left = curr.left
                     else:
                         parent.right = curr.left
-                # case 3: two children
-                else:
-                    #find min from the right subtree
-                    min_node = curr.right
-                    while min_node.left:
-                        min_node_parent = min_node
-                        min_node = min_node.left
-                    # replace curr <-> min
-                    if parent.left == curr:
-                        parent.left = min_node
+                elif curr.left is None and curr.right:
+                    if parent is None:
+                        self.root = curr.right
+                    elif parent.left == curr:
+                        parent.left = curr.right
                     else:
-                        parent.right = min_node
-                    min_node_parent.left = min_node.right if min_node.right is not None else None
-                    min_node.left = curr.left
-                    min_node.right = curr.right
-                    #delete last one
+                        parent.right = curr.right
+                # 3: target node has two children
+                else:
+                    s = curr.right      # successor
+                    sp = None           # successor's parent
+                    while s.left:
+                        sp = s
+                        s = s.left
+                    
+                    # print(str(sp))
+                    # print(str(s))
+
+                    if parent is None:
+                        if sp is not None:
+                            if s.right:
+                                sp.left = s.right
+                            else:
+                                sp.left = None
+                        s.left = self.root.left
+                        s.right = self.root.right
+                        self.root = s
+                    else:
+                        if sp is not None:
+                            if s.right:
+                                sp.left = s.right
+                            else:
+                                sp.left = None
+                        s.left = curr.left
+                        s.right = curr.right
+                        if parent.left == curr:
+                            parent.left = s
+                        else:
+                            parent.right = s
                 return True
-
-
-def traverse(node):
-    """
-    Print this function to see the BST object
-    """
-    tree = Node(node.val)
-    tree.left = None if node.left is None else traverse(node.left)
-    tree.right = None if node.right is None else traverse(node.right)
-    return tree 
